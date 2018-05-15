0fc856aae5792f97b0315870976688092b02cb0a
Thomas Barnes <barnes.thomas.c@gmail.com>
2018-05-15 21:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+sayHiToGrandma(string) {
+  if string === 
+  return "I can't hear you!"
+}
