97c54e39097b4c2f0734d3db993c43007d6d201d
Thomas Barnes <barnes.thomas.c@gmail.com>
2018-05-15 21:36:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-sayHiToGrandma(string) {
-  if string === 
-  return "I can't hear you!"
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase()
+    return "I can't hear you!"
+  else if string === string.toUpperCase()
+    return "YES INDEED!"
+  else if string === "I love you, Grandma."
+    return "I love you, too."
 }
